be671920c946e987c21ed0d256486ac0db669ddd
Oriol Prat <oriol.prat@socialpoint.es>
2014-10-24 18:30:23 +0200

    * moved some more things

---
+++ unity/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour
+{
+	public enum EState
+	{
+		Moving,
+		Attacking,
+		Dying
+	};
+
+	public EState _state;
+	public float _velocity = 1.0f;
+
+	void Start ()
+	{
+	
+	}
+
+	void Update ()
+	{
+
+	}
+
+	void FixedUpdate ()
+	{
+		switch (_state)
+		{
+		case EState.Moving:
+			FixedUpdate_Moving();
+			break;
+
+		case EState.Attacking:
+			FixedUpdate_Attacking();
+			break;
+
+		case EState.Dying:
+			FixedUpdate_Dying();
+			break;
+		}
+	}
+
+	void FixedUpdate_Moving()
+	{
+		Vector3 moveDir = new Vector3(-1,0,0);
+
+		transform.position += moveDir * _velocity * Time.fixedDeltaTime;
+	}
+
+	void FixedUpdate_Attacking()
+	{
+	}
+
+	void FixedUpdate_Dying()
+	{
+	}
+
+
+	void OnDrawGizmos ()
+	{
+		//Gizmos.DrawGUITexture(Rect(10, 10, 20, 20), myTexture);
+	}
+}
