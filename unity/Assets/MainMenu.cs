179351245850fe654ec00f7bbb29d62fa6d56981
oriolmanya <oriol.manya@socialpoint.es>
2014-10-25 02:32:19 +0200

    Main menu added

---
+++ unity/Assets/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour 
+{
+	private UIWidget me;
+	public UIButton ButtonStart;
+
+	// Use this for initialization
+	void Start () 
+	{
+		me = this.GetComponent<UIWidget>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Show(bool flag)
+	{
+		me.alpha = flag?1:0;
+		ButtonStart.enabled = flag;
+	}
+}
