fca892eb7897baf4f0e5d1401ba4bd96ca6c1822
oriolmanya <oriol.manya@socialpoint.es>
2014-10-26 08:53:57 +0100

    Fx Smoke Particle

--- unity/Assets/DestroyDelay.cs
+++ unity/Assets/DestroyDelay.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class DestroyDelay : MonoBehaviour {
 
+	public float Time = 1.0f;
+
 	// Use this for initialization
 	void Start () 
 	{
-		Destroy(this.gameObject, 1.0f);
+		Destroy(this.gameObject, Time);
 	}
 }
